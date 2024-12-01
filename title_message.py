3ee0d74ed09174c946866a6a6246f13c2d133aa8
Jose Ferreira <zepedro2001@gmail.com>
2024-12-01 19:25:30 +0000

    oi

--- title_message.py
+++ title_message.py
@@ -52,7 +52,7 @@ def get_title_message():
         if (home.text == "Tottenham" and int(home_score.text) > int(away_score.text)) or (away.text == "Tottenham" and int(away_score.text) > int(home_score.text)):
             message = "Congrats Tottenham finally won and you still need a bit of motivation, beeing a Tottenham fan must be hard."
         elif (home.text == "Tottenham" and int(home_score.text) == int(away_score.text)) or (away.text == "Tottenham" and int(away_score.text) == int(home_score.text)):
-            message = f"Tottenham draw against {opponent}, you must feel desapointed"
+            message = f"Tottenham draw against {opponent}, you must feel disappointed"
         elif (home.text == "Tottenham" and int(home_score.text) < int(away_score.text)) or (away.text == "Tottenham" and int(away_score.text) < int(home_score.text)):
             message = f"Tottenham was humiliated by {opponent}, take your daily dose of motivational pills"
 
